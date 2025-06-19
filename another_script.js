27b0af4285256d68d80a6523c9d4bd39d372c9af
Minn01 <min12200514@gmail.com>
2025-06-19 23:27:15 +0700

    added checkboxes

--- another_script.js
+++ another_script.js
@@ -14,29 +14,81 @@ const pets = [
     { "name": "Pippin", "type": "Capybara", "age": 5, "img": "petshop-img/capybaras/capybara02.jpg" },
     { "name": "Luna", "type": "Bird", "age": 2, "img": "petshop-img/birds/bird01.jpg" },
     { "name": "Jesper", "type": "Bird", "age": 4, "img": "petshop-img/birds/bird02.jpg" },
-]
+];
 
-const ap = document.getElementById("all-pets");
-let prevType = ""
-let petGroup = ""
+const activeFilters = [];
 
-pets.forEach(pet => {
-    if (pet.type != prevType) {
-        petGroup = document.createElement("div");
-        petGroup.className = "pet-group"
-        ap.innerHTML += `<h1>Type: ${pet.type}</h1>`
-        ap.appendChild(petGroup);
-        prevType = pet.type;
-    } 
-
-    let aPet = petGroup.appendChild(document.createElement("div"));
-    aPet.className = "pet";
-    aPet.innerHTML = ` 
-        <img src="${pet.img}" alt="${pet.name}"> 
-        <h3>${pet.name}</h3> 
-        <p>Type: ${pet.type}</p> 
-        <p>Age: ${pet.age} years</p> 
-        <button onclick="adoptPet()">Adopt Now</button> 
-    `; 
+$(document).ready(function () {
+    renderCheckboxes();
+    showPets();
 });
 
+function renderCheckboxes() {
+    const petForm = $("#filter-form");
+    const petTypes = [];
+    
+    pets.forEach(pet => {
+        if (!petTypes.includes(pet.type)) {
+            petTypes.push(pet.type);
+        }
+    });
+    
+    activeFilters.push(...petTypes);
+    
+    petForm.append(`<label for="type-${petTypes[0]}">Type:  </label>`);
+    
+    petTypes.forEach(petType => {
+        const inputMarkup = `
+        <input type="checkbox" name="animal-filter" id="type-${petType}" value="${petType}" checked>
+        <label for="type-${petType}">${petType}</label>
+        `;
+        petForm.append(inputMarkup);
+    });
+    
+    $("input[name='animal-filter']").on('change', function () {
+        const petType = $(this).val();
+        
+        if (this.checked) {
+            if (!activeFilters.includes(petType)) {
+                activeFilters.push(petType);
+            }
+        } else {
+            const index = activeFilters.indexOf(petType);
+            if (index > -1) {
+                activeFilters.splice(index, 1);
+            }
+        }
+        
+        showPets();
+    });
+}
+
+function showPets() {
+    const ap = $("#all-pets");
+    ap.empty();
+    let prevType = "";
+    let petGroup = null;
+    
+    pets
+        .filter(pet => activeFilters.includes(pet.type))
+        .forEach(pet => {
+            if (pet.type != prevType) {
+                petGroup = $("<div class='pet-group'></div>");
+                ap.append(`<h1>Type: ${pet.type}</h1>`);
+                ap.append(petGroup);
+                prevType = pet.type;
+            }
+            
+            const $aPet = $('<div class="pet"></div>');
+            $aPet.append(
+                ` 
+                <img src="${pet.img}" alt="${pet.name}"> 
+                <h3>${pet.name}</h3> 
+                <p>Type: ${pet.type}</p> 
+                <p>Age: ${pet.age} years</p> 
+                <button onclick="adoptPet()">Adopt Now</button> 
+                `
+            );
+            petGroup.append($aPet);
+        });
+}
